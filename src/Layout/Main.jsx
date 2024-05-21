9f459d5b770761cffdc7524cb6b8a432c8c811a5
Md.Rokonuzzaman Sayem <sayem99133@gmail.com>
2024-05-21 08:15:29 +0600

    full doctor house project setup and insatall the firebase and tailwind and daisyui and router and sweetalert and hot toast and routing done

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from "react-router-dom";
+
+const Main = () => {
+  return (
+    <div>
+      <div></div>
+      <div>
+        <Outlet></Outlet>
+      </div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Main;
